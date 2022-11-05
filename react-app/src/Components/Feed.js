62007f1f0fe9babbe68f80afe73bf551a9745d01
Samuel Tesfai <samuel.tesfai@berkeley.edu>
2022-11-04 17:30:52 -0700

    Finished

--- react-app/src/Components/Feed.js
+++ react-app/src/Components/Feed.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import Post from "./Post";
 import NewPost from "./NewPost";
 
@@ -6,18 +7,18 @@ const Feed = () => {
   // Un-comment the lines below to complete your solution
   // ====================
 
-  // const [_________, _________] = useState();
+   const [data, setData] = useState();
 
-  // const getPostsData = () => {
-  //   axios
-  //     .get(_________) //THIS IS YOUR URL OF YOUR API
-  //     .then(__________) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
-  //     .catch(__________);  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
-  // };
+   const getPostsData = () => {
+     axios
+       .get("http://localhost:3002/posts") //THIS IS YOUR URL OF YOUR API
+       .then((data)=>setData(data.data)) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
+       .catch((error) => console.log(error));  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
+   };
 
-  // useEffect(() => {
-  //   _________();
-  // }, [])
+   useEffect(() => {
+    getPostsData();
+  }, [])
 
   // ====================
 
@@ -29,7 +30,7 @@ const Feed = () => {
         )
       }
 
-      <NewPost __________ />
+      <NewPost func={getPostsData} />
     </div>
   )
 
